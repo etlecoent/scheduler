231d8fd13db258f8dd321fab42e2200ed3865e1a
Etienne LC <etlecoent@gmail.com>
2020-10-27 17:26:58 -0400

    Created all the components of an Appointment

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Header from "../Appointment/Header";
+import Show from "../Appointment/Show";
+import Empty from "../Appointment/Empty";
+
+import './styles.scss';
+
+export default function Appointment(props) {
+
+  const {id, time, interview} = props;
+
+  return (
+    <article className="appointment">
+      <Header time={time}/>
+      {interview
+        ? <Show interviewer={interview.interviewer} student={interview.student} /> 
+        : <Empty />}
+    </article>
+  )
+}
