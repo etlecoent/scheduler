60eee682902e1e4c0a3e8bb9e3fb4fe8584d277c
Etienne LC <etlecoent@gmail.com>
2020-10-28 15:49:57 -0400

    removed harcoded days & added api query with axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList.js";
 import Appointment from "components/Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
@@ -72,6 +55,15 @@ const appointments = [
 
 export default function Application(props) {
   
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const urlDays="http://localhost:8001/api/days";
+
+    axios.get(urlDays).then(response => {
+      setDays(response.data)})
+  }, [])
+
   const [day, setDay] = useState("Monday");
   
   return (
