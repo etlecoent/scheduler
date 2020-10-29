43d2da209e3127fb1d9ead8a90eed811277ba6e2
Etienne LC <etlecoent@gmail.com>
2020-10-29 15:23:30 -0400

    Stared working on the form section and currently displays empty -> create

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,21 +1,44 @@
 import React from 'react';
+import useVisualMode from "../../hooks/useVisualMode"
 
 import Header from "../Appointment/Header";
 import Show from "../Appointment/Show";
 import Empty from "../Appointment/Empty";
 
 import './styles.scss';
+import Form from './Form';
 
 export default function Appointment(props) {
-
+  
   const {time, interview} = props;
+  
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  ); 
 
   return (
     <article className="appointment">
       <Header time={time}/>
-      {interview
-        ? <Show interviewer={interview.interviewer} student={interview.student} /> 
-        : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interview={interview}
+          onCancel={() => back()}
+          onSave={() => console.log("Clicked onSave")}
+          // Hardcoded list of interviewers
+          interviewers={[]}
+        />
+      )}
     </article>
   )
 }
