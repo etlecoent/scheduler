5968171c923740460cd031475f3464cdd31c81fc
Etienne LC <etlecoent@gmail.com>
2020-10-30 17:07:30 -0400

    Implemented the separation of concerns to have a custom Hook to manage the state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList.js";
@@ -7,84 +6,19 @@ import Appointment from "components/Appointment";
 
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
+import useApplicationData from "hooks/useApplicationData";
+
 export default function Application(props) {
   
-  const [state, setState] = useState({
-    day:"Monday",
-    days:[],
-    appointments: {},
-    interviewers: {}
-  });
+  const {
+    state,
+    setDay, 
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const dailyInterviewers = getInterviewersForDay(state, state.day);
-
-  const setDay = day => setState(prev => ({...prev, day}));
-  
-  useEffect(() => {
-    const urlDays="http://localhost:8001/api/days";
-    const urlAppointments="http://localhost:8001/api/appointments";
-    const urlInterviews="http://localhost:8001/api/interviewers";
-    Promise.all([
-      axios.get(urlDays),
-      axios.get(urlAppointments),
-      axios.get(urlInterviews)
-    ]).then((all) => {
-      const [daysData, appointmentsData, interviewersData] = all;
-      const days = daysData.data;
-      const appointments = appointmentsData.data;
-      const interviewers = interviewersData.data;
-      setState(prev => ({...prev, days, appointments, interviewers}));
-    })
-  }, []);
-
-  function bookInterview(id, interview) {
-    
-    console.log(interview)
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    
-    const savedToDB = new Promise((resolve, reject) => {
-      axios({
-        method: "PUT",
-        url: `http://localhost:8001/api/appointments/${id}`,
-        data: {interview}
-      }).then((response) => {
-        setState({...state, appointments});
-        resolve(response);
-      })
-    });
-    return savedToDB;
-  }
-
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null 
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    const deletedToDB = new Promise((resolve, reject) => {
-      axios({
-        method: "DELETE",
-        url: `http://localhost:8001/api/appointments/${id}`,
-      }).then((response) => {
-        setState({...state, appointments});
-        resolve(response);
-      })
-    });
-    return deletedToDB;
-  }
 
   return (
     <main className="layout">
