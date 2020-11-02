b187d686e0a8f6827cbcb5f7d617eb1bc801a3b2
Etienne LC <etlecoent@gmail.com>
2020-11-02 14:07:53 -0500

    Added an id to the input element for test purposes

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -34,6 +34,7 @@ export default function Form(props) {
             type="text"
             placeholder="Enter Student Name"
             onChange={(event) => setName(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
